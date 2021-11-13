e368668416d7e500b59935d05f0a66f366d1bb15
Leon Neumann <leon.neumann1998@gmail.com>
2021-11-13 14:35:57 +0100

    Prettied the Arena and added world borders that are visible

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,27 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
     public GameObject target;
-    public Vector2 topLeft = new Vector2(8, 1);
-    public Vector2 bottomRight = new Vector2(14, -16);
-    // Update is called once per frame
+    public float cameraSpeed;
     void Update()
     {
         var targetPosition = target.transform.position;
-        var newPos = gameObject.transform.position;
-        if (targetPosition.x < bottomRight.x && targetPosition.x > topLeft.x)
-        {
-            newPos.x = targetPosition.x;
-        }
-        if (targetPosition.y > bottomRight.y && targetPosition.y < topLeft.y)
-        {
-            newPos.y = targetPosition.y;
-        }
-
-        gameObject.transform.position = newPos;
+        var position = gameObject.transform.position;
+        var move = new Vector2(targetPosition.x-position.x,targetPosition.y-position.y); 
+        GetComponent<Rigidbody2D>().velocity = move*cameraSpeed;
     }
 }
