b96fc323d2c111f1bc297b7fa9efd81abb26a29d
Leon Neumann <leon.neumann1998@gmail.com>
2021-11-16 11:20:46 +0100

    Added scripts

---
+++ Assets/Scripts/HealthPoints.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPoints : MonoBehaviour
+{
+    public float currentHealthPoints;
+    public float maxHealthPoints;
+    public float shieldPoints;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
