8c5ee4268e9a2c506c85a3c25b59e6d25241dcc6
reichert-p <73558016+reichert-p@users.noreply.github.com>
2022-04-06 20:30:27 +0200

    implemented pause menu in game scene functional
    sadly, still looks like ***

--- Assets/Scripts/PauseGame.cs
+++ Assets/Scripts/PauseGame.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PauseGame : MonoBehaviour
 {
-
-
+    private bool active = false; //is the MENU active?
+    public GameObject background;
     // Update is called once per frame
+    
+    private void Start()
+    {
+        Time.timeScale = 1F;
+        active = false;
+        background.SetActive(active);
+    }
     void Update()
     {
-        if (Input.GetKey(KeyCode.Escape))
+        if (Input.GetKeyUp(KeyCode.P))
         {
-            SceneManager.LoadScene(sceneBuildIndex: 2);
+            Time.timeScale = 1F - Time.timeScale;
+            active = !active;
+            background.SetActive(active);
+            new WaitForSecondsRealtime(2);
         }
     }
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: 0);
+    }
+
+    public void ResumeGame()
+    {
+        active = !active;
+        Time.timeScale = 1F - Time.timeScale;
+        background.SetActive(active);
+    }
+
 }
