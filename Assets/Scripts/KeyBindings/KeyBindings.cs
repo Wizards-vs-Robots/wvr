79eec0c46fb0ba596c7e22c01b1a652fcceeceaa
BuildTools <dominik.ochs@gmx.net>
2022-06-23 20:16:06 +0200

    fix: custom keybindings not being shipped

--- Assets/Scripts/KeyBindings/KeyBindings.cs
+++ Assets/Scripts/KeyBindings/KeyBindings.cs
@@ -14,33 +14,7 @@ public static class KeyBindings
     static KeyBindings(){
         if (!StoreKeyBindings.SetCustomBindings())
         {
-            if (!StoreKeyBindings.RestoreDefaults())
-            {
-                bindings.Add("player1_move_up", KeyCode.W);
-                bindings.Add("player1_move_left", KeyCode.A);
-                bindings.Add("player1_move_right", KeyCode.D);
-                bindings.Add("player1_move_down", KeyCode.S);
-                bindings.Add("player1_change_spell", KeyCode.E);
-                bindings.Add("player1_shoot_up", KeyCode.F);
-                bindings.Add("player1_shoot_left", KeyCode.C);
-                bindings.Add("player1_shoot_right", KeyCode.B);
-                bindings.Add("player1_shoot_down", KeyCode.V);
-                bindings.Add("player1_cooldown_action", KeyCode.X);
-                bindings.Add("pause_button", KeyCode.P);
-
-
-                // player 2
-                bindings.Add("player2_move_up", KeyCode.Semicolon);
-                bindings.Add("player2_move_left", KeyCode.BackQuote);
-                bindings.Add("player2_move_right", KeyCode.Quote);
-                bindings.Add("player2_move_down", KeyCode.Slash);
-                bindings.Add("player2_change_spell", KeyCode.Equals);
-                bindings.Add("player2_shoot_up", KeyCode.UpArrow);
-                bindings.Add("player2_shoot_left", KeyCode.LeftArrow);
-                bindings.Add("player2_shoot_right", KeyCode.RightArrow);
-                bindings.Add("player2_shoot_down", KeyCode.DownArrow);
-                bindings.Add("player2_cooldown_action", KeyCode.Minus);
-            }
+            StoreKeyBindings.RestoreDefaults();
         }
     }
 
